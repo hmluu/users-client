547427504c78d5ab33cf5b744f4ed58e20a975de
Hoa Luu <hoamluu@gmail.com>
2018-10-05 08:32:50 -0400

    fixed typo

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const LandingPage = () => (
 <div className="jumbotron">
-  <p className="display-4">Click 'View Profiles' too see the list of users!</p>
+  <p className="display-4">Click 'View Profiles' to see the list of users!</p>
   
   <p className="lead">
     <Link to="/profiles" className="btn btn-primary btn-lg" href="" role="button">View Profiles</Link>
